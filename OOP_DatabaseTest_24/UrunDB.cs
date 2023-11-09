463c83655a9d110a89e74e3d9b423f5a6c41826b
Onat Somer <89694072+OnatSoft@users.noreply.github.com>
2023-11-09 05:39:14 +0300

    V2.1 Update - UrunDB.cs

--- OOP_DatabaseTest_24/UrunDB.cs
+++ OOP_DatabaseTest_24/UrunDB.cs
@@ -9,7 +9,7 @@ namespace OOP_DatabaseTest_24
 {
     class UrunDB
     {
-        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-ONATSOFT\ONATSOFT;Initial Catalog=KitaplikOOP_DB;Integrated Security=True");
+        SqlConnection baglanti = new SqlConnection(@"connection string");
 
         public List<Urunler> UrunList()
         {// Sınıf üzerinden OOP ile veritabanı Listeleme örneği
